38ee3fd9ce0db6a8a0970715f616c26ecb78e478
oktay <okutan.oktay@gmail.com>
2022-07-29 14:37:42 +0200

    simple ui to use suggestion

---
+++ wvaConsoleInterface/Presenter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wvaConsoleInterface {
+    internal class Presenter {
+        public string SuggestionsToString(List<string> suggestions) {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var suggestion in suggestions) {
+                builder.Append(suggestion + ", ");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
--- wvaConsoleInterface/Program.cs
+++ wvaConsoleInterface/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using wva;
+using wvaConsoleInterface;
+
+Console.WriteLine("Enter Weather data:");
+Console.Write("Temperatue in Celsius: ");
+var temperature = Console.ReadLine();
+Console.Write("Wind speed in km/h: ");
+var wind = Console.ReadLine();
+Console.Write("Weather status 1=Sunny, 2=Rainy: ");
+var statusCode = Console.ReadLine();
+
+WeatherStatus status;
+
+if (int.Parse(statusCode) == 1)
+    status = WeatherStatus.Sun;
+else
+    status = WeatherStatus.Rain;
+
+WeatherData data = new WeatherData(double.Parse(temperature), int.Parse(wind), status);
+ClothingSuggestion suggester = new ClothingSuggestion(data);
+Presenter presenter = new Presenter();
+string suggestedClothing = presenter.SuggestionsToString(suggester.SuggestClothing());
+Console.WriteLine("I suggest you wear this kind of clothing: " + suggestedClothing);
